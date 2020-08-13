6f543a7edc23adef3b6a58cf7cb1a9d4e1725544
笹倉一也 <kazuya.sasakura@gmail.com>
2020-08-13 13:24:50 +0900

    login signup view

---
+++ interval/login.swift
@@ -0,0 +1,20 @@
+//
+//  login.swift
+//  interval
+//
+//  Created by 笹倉一也 on 2020/08/13.
+//  Copyright © 2020 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ interval/signup.swift
@@ -0,0 +1,20 @@
+//
+//  signup.swift
+//  interval
+//
+//  Created by 笹倉一也 on 2020/08/13.
+//  Copyright © 2020 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class signup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
